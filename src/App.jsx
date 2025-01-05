479cff1c212cc1b612bcc341a302b5241da56ff2
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2025-01-05 21:47:40 +0530

    final2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Product from "./pages/ProductPage";
+// import Product from "./pages/ProductPage";
 import Invoice from "./pages/Invoice";
 import Navbar from "./components/Navbar";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/" element={<Invoice />} />
+        <Route path="/product" element={<Invoice />} />
         <Route path="/invoice" element={<Invoice />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="*" element={<Home />} />
+        <Route path="/signin" element={<Invoice />} />
+        <Route path="/signup" element={<Invoice />} />
+        <Route path="*" element={<Invoice />} />
       </Routes>
     </BrowserRouter>
   );
